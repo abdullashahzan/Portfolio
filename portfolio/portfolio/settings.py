6ebfb475228cc69469cacb6270c5f2a9057b33d1
abdullashahzan <abdullah.getstac@gmail.com>
2024-07-31 02:16:17 +0300

    v1.0

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-e*3-zwewb91j$cxwpga8o*1ozcft35q_cn%z$f6p500(e&e-k#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['abdullahshahzan.pythonanywhere.com']
+ALLOWED_HOSTS = ['abdullahshahzan.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
